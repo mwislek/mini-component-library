07b5bbc87862fe9dc8fa43603264960cc328276d
Mike <mike.wislek@gmail.com>
2023-01-27 19:28:18 -0600

    feat: add IconInput component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,79 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const iconSize = size === "small" ? "16" : "24";
+
+  return (
+    <Container>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconContainer className={size}>
+        <Icon id={icon} size={iconSize} strokeWidth={2} />
+      </IconContainer>
+      <BaseInput
+        className={size}
+        style={{ "--width": width + "px" }}
+        placeholder={placeholder}
+      />
+    </Container>
+  );
 };
+
+const Container = styled.label`
+  display: block;
+
+  position: relative;
+
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 2px;
+
+  height: 24px;
+  margin: auto;
+  width: 24px;
+
+  &.small {
+    height: 16px;
+    width: 16px;
+  }
+`;
+
+const BaseInput = styled.input`
+  border: none;
+  border-bottom: 2px solid ${COLORS.black};
+  color: inherit;
+  font-size: ${18 / 16}rem;
+  font-weight: 700;
+  height: ${36 / 16}rem;
+  outline-offset: 2px;
+  padding-left: 36px;
+  width: var(--width);
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &.small {
+    border-bottom: 1px solid ${COLORS.black};
+    font-size: ${14 / 16}rem;
+    height: ${24 / 16}rem;
+    padding-left: 24px;
+  }
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,73 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Container>
+      <BaseSelect value={value} onChange={onChange}>
+        {children}
+      </BaseSelect>
+      <DisplayedSelect>
+        {displayedValue}
+        <IocnContainer style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" size="24" />
+        </IocnContainer>
+      </DisplayedSelect>
+    </Container>
   );
 };
 
+const Container = styled.div`
+  position: relative;
+
+  width: max-content;
+`;
+
+const BaseSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  height: 100%;
+  opacity: 0;
+  width: 100%;
+`;
+
+const DisplayedSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  font-size: 1rem;
+  font-weight: 400;
+  padding: 12px 52px 12px 16px;
+
+  ${BaseSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${BaseSelect}:hover + & {
+    color: black;
+  }
+`;
+
+
+const IocnContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+
+  height: var(--size);
+  margin: auto;
+  pointer-events: none;
+  width: var(--size);
+`;
 export default Select;
